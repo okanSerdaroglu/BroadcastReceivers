b8f38aece6a5415908a117ae0e7d653376bce614
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-03-29 11:11:00 +0300

    notes added

--- SecurityBetweenApps2/app/src/main/java/com/example/securitybetweenapps2/MyReceiver.kt
+++ SecurityBetweenApps2/app/src/main/java/com/example/securitybetweenapps2/MyReceiver.kt
@@ -5,6 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
+/** MyReceiver receives message from the same action name
+ *  intent, from other apps. Because it is declared in
+ *  AndroidManifest and exported true. If you declare
+ *  exported parameter false MyReceiver does not get
+ *  message from other apps wiht this action name
+ */
+
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "received from second App", Toast.LENGTH_LONG).show()
