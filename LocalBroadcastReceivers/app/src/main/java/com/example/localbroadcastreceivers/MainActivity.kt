d7d818d286c7371ee17a4149dd3805ecd843db04
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-03-29 12:48:37 +0300

    result receiver added in activity and called it with local manager

--- LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MainActivity.kt
+++ LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.localbroadcastreceivers
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var localBroadcastManager : LocalBroadcastManager
+    lateinit var resultReceiver: ResultReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        localBroadcastManager = LocalBroadcastManager.getInstance(this)
+        resultReceiver = ResultReceiver()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter=IntentFilter()
+        intentFilter.addAction(this.getString(R.string.result_receiver))
+        localBroadcastManager.registerReceiver(resultReceiver,intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(resultReceiver)
     }
 
     fun sendMessage(view: View) {
@@ -19,4 +41,12 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("numberTwo", 5)
         sendBroadcast(intent)
     }
+
+    open class ResultReceiver : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val sum = intent?.getIntExtra("sum",0)
+            Toast.makeText(context,sum.toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
--- LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MyReceiver.kt
+++ LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MyReceiver.kt
@@ -25,4 +25,6 @@ class MyReceiver : BroadcastReceiver() {
 
     }
 
+
+
 }
