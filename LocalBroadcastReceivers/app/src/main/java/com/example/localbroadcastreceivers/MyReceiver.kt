206c6212d1bae3c08758154a456ea8ff304d1058
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-03-29 12:26:42 +0300

    LocalBroadcastManager and result receiver added

--- LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MyReceiver.kt
+++ LocalBroadcastReceivers/app/src/main/java/com/example/localbroadcastreceivers/MyReceiver.kt
@@ -3,15 +3,25 @@ package com.example.localbroadcastreceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MyReceiver : BroadcastReceiver() {
+
+    /** firstly numberOne and numberTwo send from activity
+     *  then values read in the receiver. After that a manager
+     *  declared and result send back mainActivity from this receiver
+     */
+
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        val numberOne = intent?.getIntExtra("numberOne",0)
-        val numberTwo = intent?.getIntExtra("numberTwo",0)
+        val numberOne = intent?.getIntExtra("numberOne", 0)
+        val numberTwo = intent?.getIntExtra("numberTwo", 0)
         val sum = numberOne?.plus(numberTwo!!)
-        Toast.makeText(context,sum.toString(),Toast.LENGTH_LONG).show()
+
+        val manager = LocalBroadcastManager.getInstance(context!!)
+        val intentResult = Intent(context.getString(R.string.result_receiver))
+        intentResult.putExtra("sum", sum)
+        manager.sendBroadcast(intentResult)
 
     }
 
